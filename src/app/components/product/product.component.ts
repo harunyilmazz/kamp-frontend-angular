3349e986fe663ce7b3b7aceb72542de9e368e5b2
harunyilmazz <harunyilmaaz@gmail.com>
2022-03-26 02:09:30 +0300

    HttpClient ile backend kodlarından veriler çekildi

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import { HttpClient } from '@angular/common/http';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +9,20 @@ import { Product } from 'src/app/models/product';
   styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
+  products: Product[] = [];
+  apiUrl = 'https://localhost:44395/api/products/getall';
 
-  products:Product[] = [];
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.httpClient
+      .get<ProductResponseModel>(this.apiUrl)
+      .subscribe((response) => {
+        this.products= response.data
+      });
+  }
 }
