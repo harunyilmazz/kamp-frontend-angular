c395fa0ed9558a241025afde822ce7cc78a14f7c
harunyilmazz <harunyilmaaz@gmail.com>
2022-04-04 19:41:23 +0300

    FilterPipe ile ürün arama eklendi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ProductComponent } from './components/product/product.component';
 import { CategoryComponent } from './components/category/category.component';
 import { NaviComponent } from './components/navi/navi.component';
 import { VatAddedPipe } from './pipes/vat-added.pipe';
+import { FilterPipePipe } from './pipes/filter-pipe.pipe';
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import { VatAddedPipe } from './pipes/vat-added.pipe';
     ProductComponent,
     CategoryComponent,
     NaviComponent,
-    VatAddedPipe
+    VatAddedPipe,
+    FilterPipePipe
   ],
   imports: [
     BrowserModule,
